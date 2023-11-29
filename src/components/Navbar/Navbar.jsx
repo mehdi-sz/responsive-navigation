447a7ffdf2b0c57c156bc9a1162521c9c4f1e90a
mehdi-sz <mehdi.zayani15@gmail.com>
2023-11-29 11:51:46 +0100

    ajout du menu de navigation

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className='app__navbar'>
+        <div className="app__navbar-logo">
+            <img src="#" alt="logo" />
+        </div>
+        <ul className="app__navbar-links">
+            {['accueil', 'a propos','services','blog','contact'].map((item) => (
+                <li className='app__flex p-text' key={`link-${item}`}>
+                    <div />
+                    <a href={`#${item}`}>{item}</a>
+                </li>
+            ))}
+        </ul>
+        
+    </nav>
   )
 }
 
