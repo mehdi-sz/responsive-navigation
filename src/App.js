a07c73d421e50143861a14cfa509ae1f069fb78a
mehdi-sz <mehdi.zayani15@gmail.com>
2023-11-29 11:44:52 +0100

    structure de base du projet

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.scss'
+import { Navbar } from './components'
+const App = () => (
+    <div className='app'>
+        <Navbar />
+    </div>
+)
+
+export default App
