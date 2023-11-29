b991aca56ec5d614f44971548a7dd3cd1ff19a20
mehdi-sz <mehdi.zayani15@gmail.com>
2023-11-29 12:47:49 +0100

    ajout du menu responsive et changement du format logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 import { images } from '../../constants';
+
+import './navbar.scss'
+
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false);
+
   return (
     <nav className='app__navbar'>
         <div className="app__navbar-logo">
@@ -14,9 +21,30 @@ const Navbar = () => {
                 </li>
             ))}
         </ul>
+        <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.8, ease: 'easeIn' }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {['accueil', 'a propos','services','blog','contact'].map((item) => (
+                <li key={item}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
 
     </nav>
-  )
-}
+  );
+};
 
 export default Navbar
